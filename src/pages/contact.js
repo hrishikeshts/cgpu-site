9d09ff4fac33b098114123b62be066a740a0796c
ameya <44024565+ameyalizbeth@users.noreply.github.com>
2021-07-18 15:26:30 +0530

    contact layout

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Layout from "../components/layout";
-
+import "../styles/contact.css";
 export default function Contact() {
     return (
         <Layout>
-            <div className='page-head'>Contact Us</div>
+            <div>
+            <div className="contact-main">
+                <div className='page-head'>Contact Us</div>
+                <div className="row main">
+                    
+                    <div className="col-6 address">address</div>
+                    <div className="col-6 contac">contact</div>
+                   
+                </div>
+                <div className="reach">reach our institution</div>
+                <div className="map">google map</div>
+                </div>
+                <div className="brochure">To know more about the college and placement details, please check out our brochure</div>
+                </div>
         </Layout>
     );
 }
