6cf709f2010502f8f29d51fa984c275405d61f18
anamikaprksh <anmkprksh@gmail.com>
2021-07-20 02:55:59 +0530

    coverimage added

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Layout from "../components/layout";
+import "../styles/gallery.css";
+import coverimage from "../images/cet-front.png";
 
 export default function Gallery() {
     return (
         <Layout>
             <div className='gallery-page'>
-            
-
+            <div className="coverimg">
+                <img src={coverimage} className="coverphoto"></img>
+                {/* <h1>Gallery</h1> */}
+            </div>
+            <div>
+                
+            </div>
             </div>
         </Layout>
     );
