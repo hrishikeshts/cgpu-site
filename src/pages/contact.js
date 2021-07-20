b8821550ce5a4f25675a30c206a98bcbc88fa9fd
= <=>
2021-07-20 13:15:48 +0530

    fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,31 +9,35 @@ export default function Contact() {
                 <div className='page-head'>Contact Us</div>
                 <div className="row main p-6">
                     
-                        <div className="col-5 address">
-                            <h4>Address</h4>
-                           
-                            <p className="p-main">Career Guidance and Placement Unit (CGPU),<br/>
-                            College of Engineering Trivandrum,<br/>
-                           Engineering College P.O., Sreekaryam,<br/>
-                                Thiruvananthapuram, Kerala 695016</p>
-                            <p>
-                                Phone :<span className="phone"> 0471 2595152, 2515682</span><br />
-                                Email :<span className="phone">placement@cet.ac.in</span> 
-                            </p>
+                        <div className="col-xl-6 col-sm-12">
+                            <div className="address text-wrap">
+                                <h4>Address</h4>
+                            
+                                <p className="p-main">Career Guidance and Placement Unit (CGPU),<br/>
+                                College of Engineering Trivandrum,<br/>
+                            Engineering College P.O., Sreekaryam,<br/>
+                                    Thiruvananthapuram, Kerala 695016</p>
+                                <p>
+                                    Phone :<span className="phone"> 0471 2595152, 2515682</span><br />
+                                    Email :<span className="phone">placement@cet.ac.in</span> 
+                                </p>
+                            </div>
                             
                     </div>
-                        <div className="col-5 contac">
-                            <h4>Contact Person</h4>
-                           
-                            <p className="p-main"> Dr. K. Sunil Kumar<br/>
-                               Training and Placement Officer<br/>
-                                College of Engineering Trivandrum,<br />
-                                Thiruvananthapuram, Kerala 695016<br />
+                        <div className="col-xl-6 col-sm-12">
+                            <div className=" contac">
+                                <h4>Contact Person</h4>
+                            
+                                <p className="p-main"> Dr. K. Sunil Kumar<br/>
+                                Training and Placement Officer<br/>
+                                    College of Engineering Trivandrum,<br />
+                                    Thiruvananthapuram, Kerala 695016<br />
+                                    </p>
+                                <p>
+                                    Mobile :<span className="phone"> 9946844277</span><br />
+                                    Email :<span className="phone">placement@cet.ac.in</span> 
                                 </p>
-                            <p>
-                                Mobile :<span className="phone"> 9946844277</span><br />
-                                Email :<span className="phone">placement@cet.ac.in</span> 
-                            </p>
+                            </div>
                             
                     </div>
                    
