32a1f024262dc5b0cf44c11514f2cc677b5a1991
= <=>
2021-07-22 15:56:02 +0530

    changes2

--- src/pages/data.js
+++ src/pages/data.js
@@ -80,3 +80,14 @@ import {ibm} from '../images/ibm.png'
       url:"www.abb.com"
     },
   ]
+
+  export const objectives=[
+    {title:"Best Placements", content:"To place final year students in respectable organizations through campus recruitment"},
+    {title:"Industrial Training",content:"To provide industrial training to students during their course study"},
+    {title:"Personality & Career Development",content:"To organize lectures,seminars,group discussions,mock interviews,etc"},
+    {title:"Guide Students in the Right Path",content:"Proper mentorship to students,helping them to acheive current prospectives"},
+    {title:"Job Opportunities",content:"To find job opportunites in India and abroad"},
+    {title:"Opportunities for Self Employment",content:"To help students to work on projects,researches,and get self employed"},
+    {title:"Find Higher Studies",content:"To provide knowledge for higher studies in India and abroad"},
+    {title:"Excel in Competitive Exams",content:"To offer training for students in exams like GRE,CAT,TOEFLGMAT,GATE,etc"}
+  ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,11 @@ import "../styles/index.css";
 import logo from "../images/cet-logo.png";
 import eye from "../images/Asseteye.png";
 import missioncircle from "../images/mission.png";
+import {objectives} from './data'
 
 export default function Home() {
 
-  const objectives=[
-    {title:"Best Placements", content:"To place final year students in respectable organizations through campus recruitment"},
-    {title:"Industrial Training",content:"To provide industrial training to students during their course study"},
-    {title:"Personality & Career Development",content:"To organize lectures,seminars,group discussions,mock interviews,etc"},
-    {title:"Guide Students in the Right Path",content:"Proper mentorship to students,helping them to acheive current prospectives"},
-    {title:"Job Opportunities",content:"To find job opportunites in India and abroad"},
-    {title:"Opportunities for Self Employment",content:"To help students to work on projects,researches,and get self employed"},
-    {title:"Find Higher Studies",content:"To provide knowledge for higher studies in India and abroad"},
-    {title:"Excel in Competitive Exams",content:"To offer training for students in exams like GRE,CAT,TOEFLGMAT,GATE,etc"}
-  ]
+  
   return (
     <Layout page="home">
       {/* <div className='page-head'>Career Guidance and Placement Unit, College of Engineering Trivandrum</div>
@@ -107,9 +99,11 @@ export default function Home() {
         </div>
         <div className='midheading' style={{margin:'auto'}}>Our Objectives</div>
         <div className='row'>
-        {objectives.map(x=><div className='col-3 my-2 objectives'>
-          <div className='objectives-title'>{x.title}</div>
-          <div className='objectives-content'>{x.content}</div>
+        {objectives.map(x=><div className='col-3 my-2'>
+          <div className="objectives">
+            <div className='objectives-title'>{x.title}</div>
+            <div className='objectives-content'>{x.content}</div>
+          </div>
         </div>)}
         </div>
       </div>
