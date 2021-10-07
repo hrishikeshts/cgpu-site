9781081bfd033c42717176aff281ee5d5da0cff1
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-10-07 09:56:27 +0530

    Merge changes from Annbranch

--- src/components/data.js
+++ src/components/data.js
@@ -1,314 +1,296 @@
+export const eventData = [
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+    {
+        title: "Dr.Sunil Kumar K",
+        posi: "Training and Placement Officer",
+        mail: "placement@cet.ac.in",
+    },
+];
 
+export const recruitData = [
+    {
+        company: "Amazon",
+        url: "www.ibm.com",
+        logo: "/recruiters-logo/amazon.svg",
+    },
+    {
+        company: "intuit",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/intuit.svg",
+    },
+    {
+        company: "bosch",
+        url: "www.ibm.com",
+        logo: "/recruiters-logo/bosch.svg",
+    },
+    {
+        company: "ge",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/ge.svg",
+    },
+    {
+        company: "IBM",
+        url: "www.ibm.com",
+        logo: "/recruiters-logo/ibm.svg",
+    },
+    {
+        company: "infosys",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/infosys.svg",
+    },
+    {
+        company: "siemens",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/siemens.svg",
+    },
+    {
+        company: "amadeus",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/amadeus.svg",
+    },
+    {
+        company: "oracle",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/oracle.svg",
+    },
+    {
+        company: "Wipro",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/wipro.svg",
+    },
+    {
+        company: "cognizant",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/cognizant.svg",
+    },
+    {
+        company: "tcs",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/tcs.svg",
+    },
+    {
+        company: "tismo",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/tismo.svg",
+    },
+    {
+        company: "zoho",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/zoho.svg",
+    },
+    {
+        company: "abb",
+        url: "www.abb.com",
+        logo: "/recruiters-logo/abb.svg",
+    },
+];
 
-
-  export const eventData = [
+export const objectives = [
     {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
+        title: "Guidance of Students in the Right Path",
+        content: "To provide proper mentorship to students, helping them to acheive their career prospectives",
     },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    {
-      title:"Dr.Sunil Kumar K",
-      posi:"Training and Placement Officer",
-      mail:"placement@cet.ac.in"
-    },
-    
-  ]
-
-  export const recruitData = [
-    
-    {
-      company:"Amazon",
-      url:"www.ibm.com",
-      logo:"/recruiters-logo/amazon.svg"
-    },
-    {
-      company:"intuit",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/intuit.svg"
-    },
-    {
-      company:"bosch",
-      url:"www.ibm.com",
-      logo:"/recruiters-logo/bosch.svg"
-    },
-    {
-      company:"ge",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/ge.svg"
-    },
-    {
-      company:"IBM",
-      url:"www.ibm.com",
-      logo:"/recruiters-logo/ibm.svg"
-    },
-    {
-      company:"infosys",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/infosys.svg"
-    },
-    {
-      company:"siemens",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/siemens.svg"
-    },
-    {
-      company:"amadeus",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/amadeus.svg"
-    },
-    {
-      company:"oracle",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/oracle.svg"
-    },
-    {
-      company:"Wipro",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/wipro.svg"
-    },
-    {
-      company:"cognizant",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/cognizant.svg"
-    },
-    {
-      company:"tcs",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/tcs.svg"
-    },
-    {
-      company:"tismo",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/tismo.svg"
-    },
-     {
-      company:"zoho",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/zoho.svg"
-    },
-    {
-      company:"abb",
-      url:"www.abb.com",
-      logo:"/recruiters-logo/abb.svg"
-    },
-   
-  ]
-
-  export const objectives = [
     {
         title: "Best Placements",
-        content: "To place final year students in respectable organizations through campus recruitment",
+        content: "To place students in respectable organizations  through campus recruitment",
     },
     {
-        title: "Industrial Training",
-        content: "To provide industrial training to students during their course study",
+        title: "Higher Studies",
+        content:
+            "To give students an insight into the higher studies options in India and abroad and train them to excel in competitive exams like GRE, CAT, TOEFL, GMAT, etc.",
     },
     {
         title: "Personality & Career Development",
-        content: "To organize lectures, seminars, group discussions, mock interviews, etc.",
-    },
-    {
-        title: "Guide Students in the Right Path",
-        content: "Proper mentorship to students, helping them to acheive current prospectives",
-    },
-    { title: "Job Opportunities", content: "To find job opportunites in India and abroad" },
-    {
-        title: "Opportunities for Self Employment",
-        content: "To help students to work on projects, researches, and get self employed",
-    },
-    { title: "Find Higher Studies", content: "To provide knowledge for higher studies in India and abroad" },
-    {
-        title: "Excel in Competitive Exams",
-        content: "To offer training for students in exams like GRE, CAT, TOEFLGMAT, GATE, etc.",
+        content:
+            "To organize lectures, seminars, mock interviews and provide industrial training to students during their course study  as well as offer them opportunities to work on research projects",
     },
 ];
 export const photos = [
-  {
-    src: "/gallery-images/gimg1.jpg",
-    width: 4,
-    height: 3,
-    desc:"CET 2020"
-  },
-  {
-    src: "/gallery-images/gimg10.jpg",
-    width: 1,
-    height: 1,
-    desc:"80 Years Celebrations"
-  },
-  {
-    src: "/gallery-images/gimg7.jpg",
-    width: 1,
-    height: 1,
-    desc:"Golden Plaque by 1959 batch"
-  },
-  
-  {
-    src: "/gallery-images/gimg3.jpg",
-    width: 3,
-    height: 4,
-    desc:"CGPU Hall"
-  },
-  {
-    src: "/gallery-images/gimg2.jpg",
-    width: 1,
-    height: 1,
-    desc:"CGPU Building"
-  },
-  
-  
-  {
-    src: "/gallery-images/gimg8.jpg",
-    width: 3,
-    height: 4,
-    desc:"CET College Ground"
-  },
-  
-  {
-    src: "/gallery-images/gimg11.jpg",
-    width: 1,
-    height: 1,
-    desc:"CET Platinum Jubilee Celebrations"
-  },
-  {
-    src: "/gallery-images/gimg4.jpg",
-    width: 3,
-    height: 4,
-    desc:"CETAA Hall"
-  },
-  
-  {
-    src: "/gallery-images/gimg5.jpg",
-    width: 3,
-    height: 4,
-    desc:"Some description"
-  },
-  
-  
-  {
-    src: "/gallery-images/gimg6.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gimg12.jpg",
-    width: 4,
-    height: 3,
-    desc:"Dhwani 2019 Highlights"
-  },
-  
-  {
-    src: "/gallery-images/gim2.jpg",
-    width: 1,
-    height: 1,
-    desc:"Dept.of Architecture"
-  },
-  
-  {
-    src: "/gallery-images/gim3.jpg",
-    width: 3,
-    height: 4,
-    desc:"Graduation 2017 Batch"
-  },
-  {
-    src: "/gallery-images/gim4.jpg",
-    width: 3,
-    height: 4,
-    desc:"MBA Block"
-  },
-  
-  {
-    src: "/gallery-images/gim5.jpg",
-    width: 3,
-    height: 4,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim6.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim7.png",
-    width: 3,
-    height: 4,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim10.jpg",
-    width: 3,
-    height: 4,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim8.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gimg9.jpg",
-    width: 3,
-    height: 4,
-    desc:"Some description"
-  },
-  
-  
-  {
-    src: "/gallery-images/gim8.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim9.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim8.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  {
-    src: "/gallery-images/gim9.jpg",
-    width: 4,
-    height: 3,
-    desc:"Some description"
-  },
-  
+    {
+        src: "/gallery-images/gimg1.jpg",
+        width: 4,
+        height: 3,
+        desc: "CET 2020",
+    },
+    {
+        src: "/gallery-images/gimg10.jpg",
+        width: 1,
+        height: 1,
+        desc: "80 Years Celebrations",
+    },
+    {
+        src: "/gallery-images/gimg7.jpg",
+        width: 1,
+        height: 1,
+        desc: "Golden Plaque by 1959 batch",
+    },
+
+    {
+        src: "/gallery-images/gimg3.jpg",
+        width: 3,
+        height: 4,
+        desc: "CGPU Hall",
+    },
+    {
+        src: "/gallery-images/gimg2.jpg",
+        width: 1,
+        height: 1,
+        desc: "CGPU Building",
+    },
+
+    {
+        src: "/gallery-images/gimg8.jpg",
+        width: 3,
+        height: 4,
+        desc: "CET College Ground",
+    },
+
+    {
+        src: "/gallery-images/gimg11.jpg",
+        width: 1,
+        height: 1,
+        desc: "CET Platinum Jubilee Celebrations",
+    },
+    {
+        src: "/gallery-images/gimg4.jpg",
+        width: 3,
+        height: 4,
+        desc: "CETAA Hall",
+    },
+
+    {
+        src: "/gallery-images/gimg5.jpg",
+        width: 3,
+        height: 4,
+        desc: "Some description",
+    },
+
+    {
+        src: "/gallery-images/gimg6.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gimg12.jpg",
+        width: 4,
+        height: 3,
+        desc: "Dhwani 2019 Highlights",
+    },
+
+    {
+        src: "/gallery-images/gim2.jpg",
+        width: 1,
+        height: 1,
+        desc: "Dept.of Architecture",
+    },
+
+    {
+        src: "/gallery-images/gim3.jpg",
+        width: 3,
+        height: 4,
+        desc: "Graduation 2017 Batch",
+    },
+    {
+        src: "/gallery-images/gim4.jpg",
+        width: 3,
+        height: 4,
+        desc: "MBA Block",
+    },
+
+    {
+        src: "/gallery-images/gim5.jpg",
+        width: 3,
+        height: 4,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim6.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim7.png",
+        width: 3,
+        height: 4,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim10.jpg",
+        width: 3,
+        height: 4,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim8.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gimg9.jpg",
+        width: 3,
+        height: 4,
+        desc: "Some description",
+    },
+
+    {
+        src: "/gallery-images/gim8.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim9.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim8.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
+    {
+        src: "/gallery-images/gim9.jpg",
+        width: 4,
+        height: 3,
+        desc: "Some description",
+    },
 ];
